53da6514b0b01558a049ab4b0232c1502f48ecf2
Andy Zhang <andy@nwsoft.tk>
2022-12-03 20:13:59 +0800

    Create

---
+++ src/components/Jacksius.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Jacksius.sass";
+import $ from "jquery";
+
+export class Jacksius extends React.Component<any, any> {
+    /**
+     * Gets the temperature in Jacksius from MEGA.io
+     * @private
+     */
+
+    private temperature: string = "";
+    onResize() {
+        $("#temperature").css({"font-size": `${($(window).height() || 100) / 3}px`});
+    }
+
+    render() {
+        this.loadJacksius().then();
+
+        $(window).on("resize", this.onResize);
+        return <p id={"temperature"}>...</p>;
+    }
+
+    private async loadJacksius() {
+        const xmlHttp = new XMLHttpRequest();
+        let temperature: string = "";
+        xmlHttp.onreadystatechange = () => {
+            if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+                temperature = xmlHttp.responseText;
+                $("#temperature").text(`${temperature}°J`);
+                this.onResize()
+            }
+        }
+        xmlHttp.open("GET", "http://pst.klgrth.io/paste/ystsq/download", true); // true for asynchronous
+        xmlHttp.send(null);
+    }
+}
